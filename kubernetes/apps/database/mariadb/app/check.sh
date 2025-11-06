67abefe2011a5e889749820796a6cbeced26ac0d
ekenheim@gmail.com <ekenheim@gmail.com>
2025-11-06 17:51:10 +0000

    refactor(mariadb): update check script to use mariadb-admin and adjust health check parameters in helmrelease.yaml

--- kubernetes/apps/database/mariadb/app/check.sh
+++ kubernetes/apps/database/mariadb/app/check.sh
@@ -4,4 +4,5 @@ password_aux="${MYSQL_ROOT_PASSWORD:-}"
 if [[ -f "${MYSQL_ROOT_PASSWORD_FILE:-}" ]]; then
     password_aux=$(cat "$MYSQL_ROOT_PASSWORD_FILE")
 fi
-mysqladmin status -uroot -p"${password_aux}"
+# Use mariadb-admin instead of mysqladmin (deprecated)
+mariadb-admin status -uroot -p"${password_aux}" || mysqladmin status -uroot -p"${password_aux}"
